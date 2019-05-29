029fc9c02d280be644b66702b96b14b4684f6dab
Maartje <43209348+mflipsen@users.noreply.github.com>
2019-05-29 14:44:19 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from setup import base, case
 from general import create_project_dir, dir_case, dir_Crawler, create_setup
 from crawler import crawl_case
 from scraper import scrape_case
-from reader import read_information
 
 # =============================================================================
 # Main function to run the program
@@ -13,10 +12,13 @@ def run():
     # create a folder to store all case related files and set dir to the folder
     create_project_dir(case)
     dir_case(base, case)
+
     # execute main crawl functions
     crawl_case()
+
     # execute main scrape functions
     scrape_case()
+
     # create setup file
     create_setup()
 
@@ -27,11 +29,6 @@ def run():
 
 run()
 
-# =============================================================================
-# Optional: create separate .txt-files for each keyword
-# =============================================================================
-
-read_information()
 
 # =============================================================================
 # Optional: go back to Crawler directory
